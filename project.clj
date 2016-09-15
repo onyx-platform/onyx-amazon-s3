3f269027d4b89de78b50a4eddcc7fd0f60480806
OnyxBot <michael.drogalis@onyxplatform.org>
2016-09-15 10:38:27 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.10-beta6.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.10-beta5"]
+                 [org.onyxplatform/onyx "0.9.10-beta6"]
                  [com.amazonaws/aws-java-sdk-s3 "1.11.7"]]
   :global-vars  {*warn-on-reflection* true}
   :profiles {:circle-ci {:jvm-opts ["-Xmx2500M"
