7fb381a760cf2cc31723cb623ceec8aa3eb84460
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-12-04 23:16:04 +0800

    WIP

--- src/onyx/plugin/s3_utils.clj
+++ src/onyx/plugin/s3_utils.clj
@@ -4,13 +4,15 @@
            [com.amazonaws.services.s3 AmazonS3Client]
            [com.amazonaws.regions RegionUtils]
            [com.amazonaws.services.s3.model ObjectMetadata]
-           [java.io ByteArrayInputStream]
+           [java.io ByteArrayInputStream BufferedReader InputStreamReader]
            [com.amazonaws.services.s3.transfer TransferManager Upload]
            [com.amazonaws.event ProgressListener$ExceptionReporter]
+           [com.amazonaws.services.s3.model S3ObjectSummary]
            [com.amazonaws.services.s3.transfer.internal S3ProgressListener]
            [com.amazonaws.event ProgressEventType]
            [org.apache.commons.codec.digest DigestUtils]
            [com.amazonaws.services.s3.model PutObjectRequest]
+           [com.amazonaws.services.s3.model GetObjectRequest]
            [org.apache.commons.codec.binary Base64]))
 
 
@@ -47,3 +49,28 @@
                                        metadata)
         upload ^Upload (.upload transfer-manager put-request progress-listener)]
     upload))
+
+(defn buffered-s3-reader ^BufferedReader [^AmazonS3Client client ^String bucket ^String k]
+  (let [object (.getObject client (GetObjectRequest. bucket k))
+        reader (BufferedReader. (InputStreamReader. (.getObjectContent object)))]
+    reader))
+
+; (.readLine (buffered-s3-reader (new-client) "s3-plugin-test-05bbc495-cf56-4e7e-acb8-67a78e536e9d" "onyx.log.001"))
+
+(defn list-keys [^AmazonS3Client client ^String bucket ^String prefix]
+  (loop [listing (.listObjects client bucket prefix) ks []]
+    (let [new-ks (into ks 
+                       (map (fn [^S3ObjectSummary s] (.getKey s)) 
+                            (.getObjectSummaries listing)))]
+      (if (.isTruncated listing)
+        (recur (.listObjects client bucket prefix) new-ks)
+        new-ks))))
+
+;(list-keys (new-client) "s3-plugin-test-05bbc495-cf56-4e7e-acb8-67a78e536e9d" "") 
+
+
+;; ONLY DO ONE FILE AT A TIME
+;; THEN TRACK COUNTS, ONLY ALLOW NEXT FILE TO START AFTER PREVIOUS FILE HAS BEEN FULLY ACKED
+;; THEN YOU'LL BE ABLE TO RESUME BY DROPPING UNTIL THAT FILE, THEN SEEKING IN THAT FILE
+;; ALSO RECORD THE BUFFER OFFSET
+;; 
